25376b52ca66776d461027b0fba282e0e2bd7adb
candalistic <candalistic@gmail.com>
2018-03-24 22:52:43 -0400

    Still debugging web3

--- dist-creator/bidding.js
+++ dist-creator/bidding.js
@@ -1,11 +1,3 @@
-var web3;
-if (typeof web3 !== 'undefined') {
-  web3 = new Web3(web3.currentProvider);
-} else {
-  // set the provider you want from Web3.providers
-  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-}
-
 var nameInput = document.getElementById("name");
 var emailInput = document.getElementById("email");
 var descInput = document.getElementById("desc");
@@ -14,7 +6,24 @@ var profileInput = document.getElementById("profile");
 var deadlineInput = document.getElementById("deadline")
 var submitBtn = document.getElementById("submit");
 
+$(document).ready(function() {
+
+        //init DateTimePickers
+        materialKit.initFormExtendedDatetimepickers();
+});
+
 submitBtn.onclick = function () {
+    console.log("s " + Web3);
+    if (typeof web3 !== 'undefined') {
+        web3 = new Web3(web3.currentProvider);
+    } else {
+        // set the provider you want from Web3.providers
+        web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
+    var eth = web3.eth
+    console.log("Provider: " + web3.provider);
+    console.log(eth);
+    console.log(eth.getBlockNumber);
     name = nameInput.value;
     email = emailInput.value;
     desc = descInput.value;
@@ -22,13 +31,18 @@ submitBtn.onclick = function () {
     profile = profileInput.value;
     deadline = deadlineInput.value;
 
-    web3.eth.getBlock(5, function(error, result){
-        if(!error)
-            console.log(JSON.stringify(result));
-        else
+    eth.getBlockNumber(function(error, res) {
+        if(!error) {
+            console.log(res);
+        }
+        else {
             console.error(error);
-    })
+        }
+    });
+
+    eth.blockNumber;
+
     console.log("Creating a bid for " + name);
     jsonInterface = {}
-    contract = new web3.eth.Contract(jsonInterface)
+    // contract = new web3.eth.Contract(jsonInterface)
 };
