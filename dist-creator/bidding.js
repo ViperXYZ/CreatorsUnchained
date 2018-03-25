4c07e1c8dd8e7e8f206b99c4223171c487b6de98
AsicChen <a19c97@hotmail.com>
2018-03-24 21:10:52 -0400

    update

--- dist-creator/bidding.js
+++ dist-creator/bidding.js
@@ -6,6 +6,8 @@ if (typeof web3 !== 'undefined') {
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
 
+web3.eth.defaultAccount = web3.eth.accounts[0];
+
 var nameInput = document.getElementById("name");
 var emailInput = document.getElementById("email");
 var descInput = document.getElementById("desc");
@@ -30,5 +32,5 @@ submitBtn.onclick = function () {
     })
     console.log("Creating a bid for " + name);
     jsonInterface = {}
-    contract = new web3.eth.Contract(jsonInterface)
+    // contract = new web3.eth.Contract(jsonInterface)
 };
