ffd090929fcc89ab557b4c2af9a1f1526405ef2e
candalistic <candalistic@gmail.com>
2018-03-24 20:54:29 -0400

    Dfs

--- dist-creator/bidding.js
+++ dist-creator/bidding.js
@@ -1,4 +1,11 @@
-var web3 = new Web3(Web3.givenProvider || "ws://localhost:8546");
+var web3;
+if (typeof web3 !== 'undefined') {
+  web3 = new Web3(web3.currentProvider);
+} else {
+  // set the provider you want from Web3.providers
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+}
+
 var nameInput = document.getElementById("name");
 var emailInput = document.getElementById("email");
 var descInput = document.getElementById("desc");
@@ -8,5 +15,20 @@ var deadlineInput = document.getElementById("deadline")
 var submitBtn = document.getElementById("submit");
 
 submitBtn.onclick = function () {
-    console.log("Hello World");
+    name = nameInput.value;
+    email = emailInput.value;
+    desc = descInput.value;
+    tags = tagsInput.value;
+    profile = profileInput.value;
+    deadline = deadlineInput.value;
+
+    web3.eth.getBlock(5, function(error, result){
+        if(!error)
+            console.log(JSON.stringify(result));
+        else
+            console.error(error);
+    })
+    console.log("Creating a bid for " + name);
+    jsonInterface = {}
+    contract = new web3.eth.Contract(jsonInterface)
 };
