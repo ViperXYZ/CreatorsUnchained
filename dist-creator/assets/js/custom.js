8c87b882ca57f51a72dd7e34ac63dae906dcdae0
ViperXYZ <sammy.haque@mail.utoronto.ca>
2018-07-02 14:55:08 -0400

    moved to cdn

--- dist-creator/assets/js/custom.js
+++ dist-creator/assets/js/custom.js
@@ -1,29 +1,15 @@
-$(document).ready(function() {
-
-    //init DateTimePickers
+$(document).ready(function () {
     materialKit.initFormExtendedDatetimepickers();
-
-    // Sliders Init
-    // materialKit.initSliders();
     $('#user_transactions').hide();
 
-    $('#show_transactions').click(function() {
+    $('#show_transactions').click(function () {
         if ($('#user_transactions').is(":visible")) {
             $('#user_transactions').hide();
 
-        }else{
+        } else {
             $('#user_transactions').show();
 
         }
 
     });
-
-
-    // if (typeof web3 !== 'undefined') {
-    // web3 = new Web3(web3.currentProvider);
-    //   } else {
-    //   // set the provider you want from Web3.providers
-    //   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-    // }
-
 });
